e03c6dbb2e976210469dce632035e030b0e5451f
Shashi Kumari <126798878+shashi-kumari@users.noreply.github.com>
2025-10-13 21:33:23 +0530

    Add StateScheme enum for government scheme URLs and update GovernmentSchemesActivity

--- app/src/main/java/com/app/GovernmentSchemes/GovernmentSchemesActivity.java
+++ app/src/main/java/com/app/GovernmentSchemes/GovernmentSchemesActivity.java
@@ -8,73 +8,74 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GovernmentSchemesActivity extends BaseActivity {
     ListView stateListView;
     TextView header_title;
-    
+
     // List of all Indian states and Union Territories
     String[] indianStates = {
-        "Andhra Pradesh",
-        "Arunachal Pradesh",
-        "Assam",
-        "Bihar",
-        "Chhattisgarh",
-        "Goa",
-        "Gujarat",
-        "Haryana",
-        "Himachal Pradesh",
-        "Jharkhand",
-        "Karnataka",
-        "Kerala",
-        "Madhya Pradesh",
-        "Maharashtra",
-        "Manipur",
-        "Meghalaya",
-        "Mizoram",
-        "Nagaland",
-        "Odisha",
-        "Punjab",
-        "Rajasthan",
-        "Sikkim",
-        "Tamil Nadu",
-        "Telangana",
-        "Tripura",
-        "Uttar Pradesh",
-        "Uttarakhand",
-        "West Bengal",
-        "Andaman and Nicobar Islands",
-        "Chandigarh",
-        "Dadra and Nagar Haveli and Daman and Diu",
-        "Delhi",
-        "Jammu and Kashmir",
-        "Ladakh",
-        "Lakshadweep",
-        "Puducherry"
+            "Andhra Pradesh",
+            "Arunachal Pradesh",
+            "Assam",
+            "Bihar",
+            "Chhattisgarh",
+            "Goa",
+            "Gujarat",
+            "Haryana",
+            "Himachal Pradesh",
+            "Jharkhand",
+            "Karnataka",
+            "Kerala",
+            "Madhya Pradesh",
+            "Maharashtra",
+            "Manipur",
+            "Meghalaya",
+            "Mizoram",
+            "Nagaland",
+            "Odisha",
+            "Punjab",
+            "Rajasthan",
+            "Sikkim",
+            "Tamil Nadu",
+            "Telangana",
+            "Tripura",
+            "Uttar Pradesh",
+            "Uttarakhand",
+            "West Bengal",
+            "Andaman and Nicobar Islands",
+            "Chandigarh",
+            "Dadra and Nagar Haveli and Daman and Diu",
+            "Delhi",
+            "Jammu and Kashmir",
+            "Ladakh",
+            "Lakshadweep",
+            "Puducherry"
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_government_schemes);
-        
+
         // Setup common navigation elements
         setupCommonViews();
-        
+
         header_title = findViewById(R.id.header_title);
         header_title.setText("Government Schemes");
-        
+
         stateListView = findViewById(R.id.state_list_view);
-        
+
         // Create adapter for the list
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
-            this, 
-            android.R.layout.simple_list_item_1, 
-            indianStates
+                this,
+                android.R.layout.simple_list_item_1,
+                indianStates
         );
-        
+
         stateListView.setAdapter(adapter);
-        
+
         // Set click listener for list items
         stateListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -84,16 +85,15 @@ public class GovernmentSchemesActivity extends BaseActivity {
             }
         });
     }
-    
-    private void handleStateClick(String state) {
-        // For now, only Karnataka has a link
-        if (state.equals("Karnataka")) {
-            // Open the Karnataka government scheme URL
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, 
-                Uri.parse("https://raitamitra.karnataka.gov.in/49/policy/en"));
+
+    private void handleStateClick(String stateName) {
+        StateScheme scheme = StateScheme.fromStateName(stateName);
+        if (scheme != null && scheme.getUrl() != null && !scheme.getUrl().isEmpty()) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(scheme.getUrl()));
             startActivity(browserIntent);
+        } else {
+            Toast.makeText(this, "No scheme URL available for " + stateName, Toast.LENGTH_SHORT).show();
         }
-        // For other states, we can add more URLs in the future or show a message
     }
 
     @Override
